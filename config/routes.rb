f5764648a0d8d99a03ca4986b101342925cc67c9
jcoop32 <jcooper11@hawk.iit.edu>
2023-08-09 17:51:36 -0500

    signup page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'home#home'
+  get 'login', to: 'auth#login_form'
+  post 'login', to: 'auth#login'
+  get 'signup', to: "auth#new_user"
+  post 'signup', to: 'auth#create_user'
 end
