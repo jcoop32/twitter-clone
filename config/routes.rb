50c550b08a5aac2de3c30aab4f6162f4f68ece17
jcoop32 <jcooper11@hawk.iit.edu>
2023-08-09 19:30:37 -0500

    code refactor

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # dashboard routes
   root 'home#home'
   # login routes
-  get 'login', to: 'sessions#new_session'
-  post 'login', to: 'sessions#create_session'
-  delete 'logout', to: 'sessions#destroy'
+  get 'login', to: 'auth#new_session'
+  post 'login', to: 'auth#create_session'
+  delete 'logout', to: 'auth#destroy'
   # signup routes
   get 'signup', to: "auth#new_user"
   post 'signup', to: 'auth#create_user'
